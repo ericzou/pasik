9f81bce4deacb7b7b9dde197e79df08b523c40a0
Eric Zou <ericzou@eqlink.com>
2013-03-24 22:03:46 -0700

    WIP barChart - generated proper SVG elements, however, browser is not rendering possible bug - https://groups.google.com/forum/#!msg/angular/nP5JSuRuBMw/Wkv4DGMrtEUJ

---
+++ app/scripts/charts/barChart.js
@@ -0,0 +1,77 @@
+barChart = angular.module("pasik.charts.barChart", []);
+
+barChart.controller("BarChartController", function() {
+  var barChart;
+
+  var barWidth = 9, padding = 5;
+
+  var maxHeight = 63;
+
+  var yscale = d3.scale.linear()
+    .domain([0, 10])
+    .range([0, maxHeight]);
+
+  this.bars = [];
+
+  this.addBar = function(bar) {
+    var i;
+
+    i = this.bars.length;
+    bar.x = (barWidth + padding) * i;
+    bar.y = maxHeight - yscale(bar.value);
+    bar.width = barWidth;
+    bar.height = yscale(bar.value);
+    this.bars.push(bar);
+  };
+
+  this.setupChart = function(element) {
+
+    var width = 262, height = 63, barWidth = 9, padding = 5;
+
+    var max = 63;
+
+    barChart = d3.select(element[0])
+          .attr("width", width)
+          .attr("height", height);
+
+    barChart.selectAll(".bar")
+      .data(this.bars)
+      .enter()
+        .append('g')
+          .attr("class", "bar")
+          .append('rect')
+            .attr("x", function(d, i) { return i * (width + padding) })
+            .attr("y", function(d) { return max - d * yscale })
+            .attr("width", width)
+            .attr("height", function(d) { return d * yscale });
+  };
+});
+
+barChart.directive('barChart', function() {
+  return {
+    restrict: 'AE',
+    transclude: true,
+    controller: 'BarChartController',
+    replace: true,
+    templateUrl: "templates/charts/bar-chart.html",
+    scope: {},
+    link: function(scope, element, attrs, controller) {
+      controller.setupChart(element);
+    }
+  }
+});
+
+barChart.directive('bar', function() {
+  return {
+    restrict: 'AE',
+    require: '^barChart',
+    replace: true,
+    templateUrl: "templates/charts/bar.html",
+    scope: {
+      value: '='
+    },
+    link: function(scope, element, attrs, controller) {
+      controller.addBar(scope);
+    }
+  }
+});
---
+++ test/spec/charts/barChartSpec.js
@@ -0,0 +1,78 @@
+describe("Bar Chart", function() {
+
+  var scope, $compiler, $controller;
+
+  var barChart;
+
+  beforeEach(module('pasik.charts.barChart'));
+
+  beforeEach(inject(function(_$rootScope_, _$compile_, _$controller_) {
+    scope = _$rootScope_;
+    $compile = _$compile_;
+    $controller = _$controller_;
+
+
+  }));
+
+  describe("Static", function() {
+    beforeEach(function() {
+      var tpl =
+        "<bar-chart>" +
+           "<bar value=10>" +
+           "<bar value=10>" +
+           "<bar value=30>" +
+        "</bar-chart>"
+
+      element = angular.element(tpl);
+      $compile(element)(scope);
+      scope.$digest();
+      barChart = element.find('.bar-chart');
+    });
+
+    it("appends .bar-chart", function() {
+      dump(element);
+      expect(barChart.length).toEqual(1);
+    });
+
+    it("appends bars", function() {
+      var bars = barChart.find('.bar');
+      expect(bars.length).toEqual(3);
+    });
+  });
+
+
+  describe("Dynamic", function() {
+
+    var dataset;
+
+    beforeEach(function() {
+
+      var tpl =
+          "<bar-chart>" +
+            "<bar ng-repeat='datum in dataset' value='{{datum.value}}'></bar>" +
+          "</bar-chart>";
+
+
+      // var tpl = "<div><div class='bar' ng-repeat='datum in dataset' value='{{datum.value}}'></div></div>";
+      // dataset = [4, 7, 8, 7, 10, 2, 7, 9, 8, 7, 5, 3, 4, 3, 7, 8, 10, 7, 3, 5]
+      dataset = [
+        { label: "a", value: 10 },
+        { label: "b", value: 20 }
+      ]
+      element = angular.element(tpl);
+      $compile(element)(scope);
+      scope.$digest();
+    });
+
+    it("creats .bar for every element in dataset", function() {
+      var bars;
+      scope.dataset = dataset;
+
+      scope.$digest();
+      dump(element);
+      bars = element.find('.bar');
+      expect(bars.length).toEqual(dataset.length);
+    });
+  });
+
+});
